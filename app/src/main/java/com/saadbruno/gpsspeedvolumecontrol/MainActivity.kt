6595c8f9ba118dac9565ae9a7e6ffb26b66c4954
saadbruno <saadbruno@gmail.com>
2024-04-06 20:22:26 -0300

    converted to lambda, whatever that means

--- app/src/main/java/com/saadbruno/gpsspeedvolumecontrol/MainActivity.kt
+++ app/src/main/java/com/saadbruno/gpsspeedvolumecontrol/MainActivity.kt
@@ -3,7 +3,6 @@ package com.saadbruno.gpsspeedvolumecontrol
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
@@ -12,7 +11,6 @@ import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,10 +18,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.saadbruno.gpsspeedvolumecontrol.ui.theme.GPSSpeedVolumeControlTheme
@@ -52,15 +48,7 @@ fun Speedometer() {
     val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
     val speed = remember { mutableFloatStateOf(0f) }
 
-    val locationListener = object : LocationListener {
-        override fun onLocationChanged(location: Location) {
-            speed.floatValue = location.speed
-        }
-
-      //  override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-      //  override fun onProviderEnabled(provider: String) {}
-      //  override fun onProviderDisabled(provider: String) {}
-    }
+    val locationListener = LocationListener { location -> speed.floatValue = location.speed }
 
     BackHandler {
         locationManager.removeUpdates(locationListener)
