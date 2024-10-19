b544a241b7de15986b0b320381bd12e5d0ce2919
Bruno Saad <saadbruno@gmail.com>
2024-10-18 22:37:18 -0300

    added hash code to the view model log, so we can track if they're the same instance or not

--- app/src/main/java/com/saadbruno/gpsspeedvolumecontrol/SpeedViewModel.kt
+++ app/src/main/java/com/saadbruno/gpsspeedvolumecontrol/SpeedViewModel.kt
@@ -11,8 +11,12 @@ class SpeedViewModel : ViewModel() {
     private val _speed = MutableStateFlow(0f) // Initial speed
     val speed: StateFlow<Float> get() = _speed
 
+    init {
+        Log.d("SpeedViewModel", "ViewModel instance created. Hash code: ${System.identityHashCode(this)}")
+    }
+
     fun updateSpeed(newSpeed: Float) {
-        Log.d("SpeedViewModel", "Updating speed to $newSpeed")
+        Log.d("SpeedViewModel", "Updating speed to $newSpeed (Hash code: ${System.identityHashCode(this)})")
         viewModelScope.launch {
             _speed.value = newSpeed
         }
